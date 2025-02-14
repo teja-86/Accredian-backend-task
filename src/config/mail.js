0a2395acd571a1fd628118dcaea1ebf466dbfb88
Ranga Dharma Teja Kuntumalla <rangadharmateja0@gmail.com>
2025-02-14 10:46:11 +0530

    Added mailer

--- src/config/mail.js
+++ src/config/mail.js
@@ -4,6 +4,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const transporter = nodeMailer.createTransport({
+    secure: true,
+    host: "smtp.gmail.com",
+    port: 465,
     service: "gmail",
     auth: {
         user: process.env.GMAIL_USER,
